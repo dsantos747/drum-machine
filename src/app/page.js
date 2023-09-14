c6bf0ea6e8bc3f12d1dcf64df8624b1c74d4f286
dsantos747 <d.santos74747@gmail.com>
2023-09-14 19:27:18 +0100

    Feat: Style improvements, incorporated Howler for audio handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,97 +1,141 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
+
+// Howler.ctx.resume();
+Howler.volume(1.0);
+Howler.autoSuspend = false;
 
 function DrumMachine() {
-  const [audioFiles] = useState([
-    { id: "Q", src: "/kit_1_fx_2.wav" },
-    { id: "W", src: "/kit_1_fx_3.wav" },
-    { id: "E", src: "/kit_1_fx_4.wav" },
-    { id: "A", src: "/kit_1_open_hh.wav" },
-    { id: "S", src: "/kit_1_clap.wav" },
-    { id: "D", src: "/kit_1_fx_1.wav" },
-    { id: "Z", src: "/kit_1_kick.wav" },
-    { id: "X", src: "/kit_1_snare.wav" },
-    { id: "C", src: "/kit_1_closed_hh.wav" },
-  ]);
+  // const [audioFiles, setAudioFiles] = useState(selectBank());
+  const [audioFiles, setAudioFiles] = useState(null);
+  // const [audioFiles, setAudioFiles] = useState([
+  //   { id: "Q", src: "/kit_1_fx_2.wav" },
+  //   { id: "W", src: "/kit_1_fx_3.wav" },
+  //   { id: "E", src: "/kit_1_fx_4.wav" },
+  //   { id: "A", src: "/kit_1_open_hh.wav" },
+  //   { id: "S", src: "/kit_1_clap.wav" },
+  //   { id: "D", src: "/kit_1_fx_1.wav" },
+  //   { id: "Z", src: "/kit_1_kick.wav" },
+  //   { id: "X", src: "/kit_1_snare.wav" },
+  //   { id: "C", src: "/kit_1_closed_hh.wav" },
+  // ]);
 
-  // function handlePadClick(id) {
-  //   playClip(id);
-  // }
+  const selectBank = (bank) => {
+    switch (bank) {
+      case "bank-2":
+        break;
+      default:
+        setAudioFiles([
+          { id: "Q", src: new Howl({ src: ["/kit_1_fx_2.wav"] }) },
+          { id: "W", src: new Howl({ src: ["/kit_1_fx_3.wav"] }) },
+          { id: "E", src: new Howl({ src: ["/kit_1_fx_4.wav"] }) },
+          { id: "A", src: new Howl({ src: ["/kit_1_open_hh.wav"] }) },
+          { id: "S", src: new Howl({ src: ["/kit_1_clap.wav"] }) },
+          { id: "D", src: new Howl({ src: ["/kit_1_fx_1.wav"] }) },
+          { id: "Z", src: new Howl({ src: ["/kit_1_kick.wav"] }) },
+          { id: "X", src: new Howl({ src: ["/kit_1_snare.wav"] }) },
+          { id: "C", src: new Howl({ src: ["/kit_1_closed_hh.wav"] }) },
+        ]);
+    }
+  };
+
+  const sound = new Howl({ src: ["/kit_1_fx_2.wav"] });
 
   useEffect(() => {
+    selectBank();
+
     function handleKeyDown(event) {
       const keyToPad = {
-        q: "Q",
-        w: "W",
-        e: "E",
-        a: "A",
-        s: "S",
-        d: "D",
-        z: "Z",
-        x: "X",
-        c: "C",
+        q: 1,
+        w: 2,
+        e: 3,
+        a: 4,
+        s: 5,
+        d: 6,
+        z: 7,
+        x: 8,
+        c: 9,
       };
 
       const padId = keyToPad[event.key.toLowerCase()];
 
       if (padId) {
-        playClip(padId);
+        playClip(padId - 1, event.key.toUpperCase());
       }
     }
 
-    // Add the event listener when the component mounts
     document.addEventListener("keydown", handleKeyDown);
 
-    // Remove the event listener when the component unmounts
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
 
-  function playClip(id) {
-    const audioObj = document.getElementById(id);
-    if (audioObj) {
-      audioObj.play();
-      document.getElementById("display-text").textContent = audioObj.getAttribute("name");
-    }
+  function playClip(id, pad_id) {
+    audioFiles[id]["src"].play();
+    document.getElementById("display-text").textContent = document.getElementById(pad_id).getAttribute("name");
   }
 
   return (
     <main id="drum-machine" className={styles.main}>
-      <div className={styles.body}>
-        <div id="display" className={styles.display}>
+      <div className="banner">
+        <p className="bannerText">
+          Use letter keys to trigger pads.&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Product under
+          development.&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;If you want more features, check back soon.
+        </p>
+      </div>
+      <div className="body">
+        <div id="display" className="display">
           <p id="display-text"></p>
         </div>
         <div id="pads" className={styles.pads}>
-          <div id="heater-2" className="drum-pad" onClick={() => playClip("Q")}>
-            <audio name="Heater 2" src="/kit_1_fx_2.wav" className="clip" id="Q"></audio>Q
+          <div id="heater-2" className="drumPad" onClick={() => playClip(0, "Q")}>
+            <audio name="Heater 2" src="/kit_1_fx_2.wav" className="clip" id="Q"></audio>
+            <p className="padLetter">Q</p>
+            <p className="padNumber">1</p>
           </div>
-          <div id="heater-3" className="drum-pad" onClick={() => playClip("W")}>
-            <audio name="Heater 3" src="/kit_1_fx_3.wav" className="clip" id="W"></audio>W
+          <div id="heater-3" className="drumPad" onClick={() => playClip(1, "W")}>
+            <audio name="Heater 3" src="/kit_1_fx_3.wav" className="clip" id="W"></audio>
+            <p className="padLetter">W</p>
+            <p className="padNumber">2</p>
           </div>
-          <div id="heater-4" className="drum-pad" onClick={() => playClip("E")}>
-            <audio name="Heater 4" src="/kit_1_fx_4.wav" className="clip" id="E"></audio>E
+          <div id="heater-4" className="drumPad" onClick={() => playClip(2, "E")}>
+            <audio name="Heater 4" src="/kit_1_fx_4.wav" className="clip" id="E"></audio>
+            <p className="padLetter">E</p>
+            <p className="padNumber">3</p>
           </div>
-          <div id="open-hh" className="drum-pad" onClick={() => playClip("A")}>
-            <audio name="Open Hi-Hat" src="/kit_1_open_hh.wav" className="clip" id="A"></audio>A
+          <div id="open-hh" className="drumPad" onClick={() => playClip(3, "A")}>
+            <audio name="Open Hi-Hat" src="/kit_1_open_hh.wav" className="clip" id="A"></audio>
+            <p className="padLetter">A</p>
+            <p className="padNumber">4</p>
           </div>
-          <div id="clap" className="drum-pad" onClick={() => playClip("S")}>
-            <audio name="Clap" src="/kit_1_clap.wav" className="clip" id="S"></audio>S
+          <div id="clap" className="drumPad" onClick={() => playClip(4, "S")}>
+            <audio name="Clap" src="/kit_1_clap.wav" className="clip" id="S"></audio>
+            <p className="padLetter">S</p>
+            <p className="padNumber">5</p>
           </div>
-          <div id="heater-1" className="drum-pad" onClick={() => playClip("D")}>
-            <audio name="Heater 1" src="/kit_1_fx_1.wav" className="clip" id="D"></audio>D
+          <div id="heater-1" className="drumPad" onClick={() => playClip(5, "D")}>
+            <audio name="Heater 1" src="/kit_1_fx_1.wav" className="clip" id="D"></audio>
+            <p className="padLetter">D</p>
+            <p className="padNumber">6</p>
           </div>
-          <div id="kick" className="drum-pad" onClick={() => playClip("Z")}>
-            <audio name="Kick" src="/kit_1_kick.wav" className="clip" id="Z"></audio>Z
+          <div id="kick" className="drumPad" onClick={() => playClip(6, "Z")}>
+            <audio name="Kick" src="/kit_1_kick.wav" className="clip" id="Z"></audio>
+            <p className="padLetter">Z</p>
+            <p className="padNumber">7</p>
           </div>
-          <div id="snare" className="drum-pad" onClick={() => playClip("X")}>
-            <audio name="Snare" src="/kit_1_snare.wav" className="clip" id="X"></audio>X
+          <div id="snare" className="drumPad" onClick={() => playClip(7, "X")}>
+            <audio name="Snare" src="/kit_1_snare.wav" className="clip" id="X"></audio>
+            <p className="padLetter">X</p>
+            <p className="padNumber">8</p>
           </div>
-          <div id="closed-hh" className="drum-pad" onClick={() => playClip("C")}>
-            <audio name="Closed Hi-Hat" src="/kit_1_closed_hh.wav" className="clip" id="C"></audio>C
+          <div id="closed-hh" className="drumPad" onClick={() => playClip(8, "C")}>
+            <audio name="Closed Hi-Hat" src="/kit_1_closed_hh.wav" className="clip" id="C"></audio>
+            <p className="padLetter">C</p>
+            <p className="padNumber">9</p>
           </div>
         </div>
       </div>
