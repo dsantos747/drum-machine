03e63b90470c5448e52526473870b3021316b50b
dsantos747 <d.santos74747@gmail.com>
2023-09-15 13:01:54 +0100

    Fix: refactored to fix useEffect dependency array

--- src/app/page.js
+++ src/app/page.js
@@ -48,38 +48,28 @@ function DrumMachine() {
     }
   };
 
+  const keyToPad = { "Q": 0, "W": 1, "E": 2, "A": 3, "S": 4, "D": 5, "Z": 6, "X": 7, "C": 8 };
+
   useEffect(() => {
     function handleKeyDown(event) {
-      const keyToPad = {
-        q: 1,
-        w: 2,
-        e: 3,
-        a: 4,
-        s: 5,
-        d: 6,
-        z: 7,
-        x: 8,
-        c: 9,
-      };
-
-      const padId = keyToPad[event.key.toLowerCase()];
-
-      if (padId) {
-        playClip(padId - 1, event.key.toUpperCase());
+      if (keyToPad.hasOwnProperty(event.key.toUpperCase())) {
+        playClip(event.key.toUpperCase());
       }
     }
 
     document.addEventListener("keydown", handleKeyDown);
-
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  });
+  }, [playClip("noSoundWasHeard")]);
 
-  function playClip(id, padLetter) {
-    audioFiles[id]["src"].play();
-    setActiveSounds({ ...activeSounds, [padLetter]: id });
-    document.getElementById("display-text").textContent = document.getElementById(padLetter).getAttribute("name");
+  function playClip(padLetter) {
+    if (keyToPad.hasOwnProperty(padLetter)) {
+      const index = keyToPad[padLetter];
+      audioFiles[index]["src"].play();
+      setActiveSounds({ ...activeSounds, [padLetter]: index });
+      document.getElementById("display-text").textContent = document.getElementById(padLetter).getAttribute("name");
+    }
   }
 
   function stopClip(padLetter) {
@@ -112,47 +102,47 @@ function DrumMachine() {
             <p id="display-text"></p>
           </div>
           <div id="pads" className={styles.pads}>
-            <div id="heater-2" className="drumPad inactive" onMouseDown={() => playClip(0, "Q")} onMouseUp={() => stopClip("Q")}>
+            <div id="heater-2" className="drumPad inactive" onMouseDown={() => playClip("Q")} onMouseUp={() => stopClip("Q")}>
               <audio name="Heater 2" src="/kit_1_fx_2.wav" className="clip" id="Q"></audio>
               <p className="padLetter">Q</p>
               <p className="padNumber">1</p>
             </div>
-            <div id="heater-3" className="drumPad inactive" onMouseDown={() => playClip(1, "W")} onMouseUp={() => stopClip("W")}>
+            <div id="heater-3" className="drumPad inactive" onMouseDown={() => playClip("W")} onMouseUp={() => stopClip("W")}>
               <audio name="Heater 3" src="/kit_1_fx_3.wav" className="clip" id="W"></audio>
               <p className="padLetter">W</p>
               <p className="padNumber">2</p>
             </div>
-            <div id="heater-4" className="drumPad inactive" onMouseDown={() => playClip(2, "E")} onMouseUp={() => stopClip("E")}>
+            <div id="heater-4" className="drumPad inactive" onMouseDown={() => playClip("E")} onMouseUp={() => stopClip("E")}>
               <audio name="Heater 4" src="/kit_1_fx_4.wav" className="clip" id="E"></audio>
               <p className="padLetter">E</p>
               <p className="padNumber">3</p>
             </div>
-            <div id="open-hh" className="drumPad inactive" onMouseDown={() => playClip(3, "A")} onMouseUp={() => stopClip("A")}>
+            <div id="open-hh" className="drumPad inactive" onMouseDown={() => playClip("A")} onMouseUp={() => stopClip("A")}>
               <audio name="Open Hi-Hat" src="/kit_1_open_hh.wav" className="clip" id="A"></audio>
               <p className="padLetter">A</p>
               <p className="padNumber">4</p>
             </div>
-            <div id="clap" className="drumPad inactive" onMouseDown={() => playClip(4, "S")} onMouseUp={() => stopClip("S")}>
+            <div id="clap" className="drumPad inactive" onMouseDown={() => playClip("S")} onMouseUp={() => stopClip("S")}>
               <audio name="Clap" src="/kit_1_clap.wav" className="clip" id="S"></audio>
               <p className="padLetter">S</p>
               <p className="padNumber">5</p>
             </div>
-            <div id="heater-1" className="drumPad inactive" onMouseDown={() => playClip(5, "D")} onMouseUp={() => stopClip("D")}>
+            <div id="heater-1" className="drumPad inactive" onMouseDown={() => playClip("D")} onMouseUp={() => stopClip("D")}>
               <audio name="Heater 1" src="/kit_1_fx_1.wav" className="clip" id="D"></audio>
               <p className="padLetter">D</p>
               <p className="padNumber">6</p>
             </div>
-            <div id="kick" className="drumPad inactive" onMouseDown={() => playClip(6, "Z")} onMouseUp={() => stopClip("Z")}>
+            <div id="kick" className="drumPad inactive" onMouseDown={() => playClip("Z")} onMouseUp={() => stopClip("Z")}>
               <audio name="Kick" src="/kit_1_kick.wav" className="clip" id="Z"></audio>
               <p className="padLetter">Z</p>
               <p className="padNumber">7</p>
             </div>
-            <div id="snare" className="drumPad inactive" onMouseDown={() => playClip(7, "X")} onMouseUp={() => stopClip("X")}>
+            <div id="snare" className="drumPad inactive" onMouseDown={() => playClip("X")} onMouseUp={() => stopClip("X")}>
               <audio name="Snare" src="/kit_1_snare.wav" className="clip" id="X"></audio>
               <p className="padLetter">X</p>
               <p className="padNumber">8</p>
             </div>
-            <div id="closed-hh" className="drumPad inactive" onMouseDown={() => playClip(8, "C")} onMouseUp={() => stopClip("C")}>
+            <div id="closed-hh" className="drumPad inactive" onMouseDown={() => playClip("C")} onMouseUp={() => stopClip("C")}>
               <audio name="Closed Hi-Hat" src="/kit_1_closed_hh.wav" className="clip" id="C"></audio>
               <p className="padLetter">C</p>
               <p className="padNumber">9</p>
